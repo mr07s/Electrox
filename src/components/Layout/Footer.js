a61ca2f5d0d751b36aaa12656c88806c556daa10
mr07s <106861707+mr07s@users.noreply.github.com>
2023-06-30 20:04:50 +0530

    project initialized

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <>
+      <div className="container">
+        <h4>All rights reserver &copy; sg</h4>
+        <div className="footer_text_container">
+        <p className="footer_text">
+          <Link  className="footer_link" to="/about   ">About</Link>
+        </p>
+        |
+        <p className="footer_text">
+          <Link className="footer_link" to="/about">Contact</Link>
+        </p>
+        |
+        <p className="footer_text">
+          <Link className="footer_link" to="/about">PrivacyPolicy</Link>
+        </p>
+        |
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
