e18da3e303f52cff7afe6ebd6b27a9d4879e39fe
mr07s <106861707+mr07s@users.noreply.github.com>
2024-08-19 03:54:08 +0530

    UI fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import PrivateRoute from "./components/Layout/Routes/Private";
 // import Private from "./components/Layout/Routes/Private";
 import About from "./pages/About";
@@ -8,7 +7,7 @@ import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Pagenotfound from "./pages/Pagenotfound";
 import Policy from "./pages/Policy";
-import Register from './pages/Register'
+import Register from "./pages/Register";
 import Dasboard from "./pages/user/UserDasboard";
 import ForgotPassword from "./pages/ForgotPassword";
 import AdminRoute from "./components/Layout/Routes/AdminRoute";
@@ -30,50 +29,40 @@ import AdminOrders from "./pages/admin/AdminOrders";
 function App() {
   return (
     <>
-    <Routes>
-<Route path='/' element={<HomePage/>}/>
-<Route path='/product/:slug' element={<ProductDetails/>}/>
-<Route path='/search' element={<Search/>}/>
-<Route path='/cart' element={<CartPage/>}/>
-<Route path='/categories' element={<Categories/>}/>
-<Route path='/category/:slug' element={<CategoryProduct/>}/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:slug" element={<ProductDetails />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/category/:slug" element={<CategoryProduct />} />
 
-<Route  path="/dashboard" element={<PrivateRoute/>}  >
-<Route path='user' element={<Dasboard/>}/>
-<Route path='user/oders' element={<Oders/>}/>
-<Route path='user/profile' element={<Profile/>}/>
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="user" element={<Dasboard />} />
+          <Route path="user/oders" element={<Oders />} />
+          <Route path="user/profile" element={<Profile />} />
+        </Route>
 
-</Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/product/:slug" element={<UpdateProduct />} />
+          <Route path="admin/products" element={<Product />} />
+          <Route path="admin/users" element={<Users />} />
+          <Route path="admin/orders" element={<AdminOrders />} />
+        </Route>
 
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot_password" element={<ForgotPassword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
 
-<Route  path="/dashboard" element={<AdminRoute/>}  >
-<Route path='admin' element={<AdminDashboard/>}/>
-<Route path='admin/create-category' element={<CreateCategory/>}/>
-<Route path='admin/create-product' element={<CreateProduct/>}/>
-<Route path='admin/product/:slug' element={<UpdateProduct/>}/>
-<Route path='admin/products' element={<Product/>}/>
-<Route path='admin/users' element={<Users/>}/>
-<Route path='admin/orders' element={<AdminOrders/>}/>
-
-</Route>
-
-
-
-<Route path='/about' element={<About/>}/>
-<Route path='/register' element={<Register/>}/>
-<Route path='/forgot_password' element={<ForgotPassword/>}/>
-<Route path='/login' element={<Login/>}/>
-<Route path='/contact' element={<Contact/>}/>
-<Route path='/policy' element={<Policy/>}/>
-<Route path='*' element={<Pagenotfound/>}/>
-
-    </Routes>
-    
-    <>
-
-   
-
-    </>
+      <></>
     </>
   );
 }
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -3,12 +3,13 @@ import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout/Layout/Layout";
 import { Link, useParams } from "react-router-dom";
 import MuiCard from "../components/Layout/MeterialUiComponents/Card";
+import { Button } from "@mui/material";
 const ProductDetails = () => {
   const params = useParams();
   // getProduct
   const [product, setProduct] = useState({});
   const [relatedproduct, setRelatedproduct] = useState([]);
-//   console.log(product);
+  //   console.log(product);
 
   const getProduct = async () => {
     try {
@@ -16,84 +17,119 @@ const ProductDetails = () => {
         `${process.env.REACT_APP_API}/api/v1/products/get-product/${params.slug}`
       );
       setProduct(data?.singleProduct);
-    //   console.log(data?.singleProduct);
-      getSimilarProducts(data?.singleProduct._id,data?.singleProduct.category._id);
+      //   console.log(data?.singleProduct);
+      getSimilarProducts(
+        data?.singleProduct._id,
+        data?.singleProduct.category._id
+      );
     } catch (error) {
       console.log(error);
     }
   };
 
-
-const getSimilarProducts =async(pid,cid)=>{
-
-try{
-const {data} =await axios.get(`${process.env.REACT_APP_API}/api/v1/products/related-product/${pid}/${cid}`);
-console.log({data})
-setRelatedproduct(data?.products);
-}
-catch(error){
-
-console.log(error);
-
-
-
-
-}
-
-
-
-
-}
-
-
+  const getSimilarProducts = async (pid, cid) => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/products/related-product/${pid}/${cid}`
+      );
+      console.log({ data });
+      setRelatedproduct(data?.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   // initial p details
   useEffect(() => {
     if (params?.slug) {
       getProduct();
     }
-// eslint-disable-next-line
+    // eslint-disable-next-line
   }, [params?.slug]);
 
   return (
     <Layout>
-      <h1>Product Detail</h1>
+      <div style={{ paddingTop: ".7rem" }}>
+        <h1>Product Detail</h1>
+      </div>
 
-      <div className="product_details">
-        <div className="image_container" style={{ height: "200px" }}>
+      <div
+        className="product_details"
+        style={{
+          // paddingTop: "2rem",
+          // border: "1px solid black",
+          display: "flex",
+          justifyContent: "center",
+          minHeight: "25rem",
+          padding: "2rem",
+          flexWrap: "wrap",
+          // background: "green",
+        }}
+      >
+        <div
+          className="image_container"
+          style={{
+            height: "20rem",
+            margin: "2rem",
+            minWidth: "40%",
+            display: "flex",
+            // background: "black",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <img
             src={`${process.env.REACT_APP_API}/api/v1/products/product-photo/${product?._id}`}
             alt={`${product.name}`}
-            style={{ height: "200px" }}
+            style={{ height: "100%", width: "100%" }}
           />
         </div>
-        <div className="details_container">
-<h1>Product Details</h1>
-<h3>{product?.name}</h3>
-<h3>{product?.description}</h3>
-<h3>{`$${product?.price}`}</h3>
-<h3>{product?.category?.name}</h3>
-<button className="btn btn_addtocart">Add to cart</button>
-
+        <div
+          className="details_container"
+          style={{
+            minHeight: "25rem",
+            // background: "red",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            minWidth: "30%",
+          }}
+        >
+          {/* <h1>Product Details</h1> */}
+          <h3 style={{ marginBottom: "1.2rem" }}>{product?.name}</h3>
+          <h3 style={{ marginBottom: "1.2rem" }}>{product?.description}</h3>
+          <h3 style={{ marginBottom: "1.2rem" }}>{`$${product?.price}`}</h3>
+          <h3 style={{ marginBottom: "1.2rem" }}> {product?.category?.name}</h3>
+          <Button
+            className="btn btn_addtocart"
+            style={{ marginBottom: "1.2rem", color: "black" }}
+          >
+            Add to cart
+          </Button>
         </div>
-
-
-
-
-
-
       </div>
-      <div className="similar_products">
-        <h1>Similar Products</h1>
-        {relatedproduct.length <1 && <p>No similar Product Found</p>}
-      {
-      relatedproduct?.map((p)=>(
-       
-        <MuiCard product_name={p.name} key={p._id}  products_description={p.description}  product_price={p.price} product_id={p._id} product_slug={p.slug}/>
-        
-      ))
-    }
-
+      <h1>Similar Products</h1>
+      <div
+        className="similar_products"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          padding: "2rem",
+          flexWrap: "wrap",
+        }}
+      >
+        {relatedproduct.length < 1 && <p>No similar Product Found</p>}
+        {relatedproduct?.map((p) => (
+          <MuiCard
+            product_name={p.name}
+            key={p._id}
+            products_description={p.description}
+            product_price={p.price}
+            product_id={p._id}
+            product_slug={p.slug}
+          />
+        ))}
       </div>
     </Layout>
   );
